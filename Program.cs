f6d0b507602ff4d099e0bb8c3e9faefb4ebb9409
osama-moh-ramadan <osamamar69@gmail.com>
2025-05-07 12:03:58 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,80 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace section
+namespace nested_classes
 {
-    class Human
-    {
-        public int age;
-        public string name;
-        public Human()
-        {
-            Console.WriteLine("....human created.....");
-        }
-        public Human(string name, int age)
-        {
-            this.name = name;
-            this.age = age;
-        }
-        public void printhuman()
-        {
-
-            Console.Write($"Name is {name}, and age is {age}",name,age);
-        }
-    }
-    class Student :Human 
-    {
-        public double grades;
-        public Student(string name, int age,double grades):base(name,age) 
-        {
-            this.grades = grades;
-        }
-        public Student()
-        {
-            Console.WriteLine(".....student created.....");
-        }
-        public void printstudent()
-        {
-            printhuman();
-            Console.WriteLine($",grades is {grades}",grades);
-        }
-    }
-    class Teatcher : Human
-    {
-        public string course;
-        public double salary;
-        public Teatcher(string name,int age,string course, double salary):base(name,age)
-        {
-            this.course = course;
-            this.salary = salary;
-        }
-
-        public Teatcher()
-        {
-            Console.WriteLine("....Teacher created.....");
-        }
-        public void printteatcher()
-        {
-            printhuman();
-            Console.WriteLine($" and salary is {salary} and course is {course}",salary,course);
-
-        }
-    } 
-   
     internal class Program
-    { 
-     
+    {
+        // abstract classes : we cannot create an object from it
+        // we use it for inheritance only 
+        // we have to create a child class and create an abject form it 
+        //sealed classes 
+        // we cannot make inheritance in sealed classes 
+        // sealed is opposite to abstract
+        // c# donnot allow us to inheritance from many classes 
+        abstract class Person
+         { 
+            public string name;
+            public int age;
+            public void Hi()
+            {
+                Console.WriteLine("Hi and welcome");
+            }
+            public abstract void PrinInfo();
+        }
+        class Student : Person , Iprint , Ihello
+        {
+           public double grades;
+            public hi()
+            {
+                Console.WriteLine("say hi from strdent");
+            }
+            public void print()
+            {
+                Console.WriteLine("print infob");
+            }
+
+            public override void PrinInfo()
+            {
+                Console.WriteLine($"name is {name}, and age = {age} , and grades = {grades}");
+            }
+        }
+        public interface Iprint
+        {
+            void print();
+        }
+        public interface Ihello
+        {
+            void hi();
+        }
+
         static void Main(string[] args)
         {
-            Human h1 = new Human("osama ramdan", 23);
-            h1.printhuman();
-            Teatcher t1 = new Teatcher("ahmed mahmoud", 55,"oop",10000);
-            t1.printteatcher();
-            Student s1 = new Student("ahmed",15,89);
-            s1.printstudent();
+           
+            //Student s1 = new Student();
+            //s1.name = "osama";
+            //s1.grades = 99;
+            //s1.age = 19;
+            //s1.PrinInfo();
+
         }
     }
 }
---
+++ method overriding.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boxing
+{
+    class Human
+    {
+        public string Name;
+        public int age;
+        protected string nationality;
+
+        public Human(string name, int age)
+        {
+            this.Name = name;
+            this.age = age;
+        }
+        public virtual void printinfo()
+        {
+            Console.WriteLine("Hman");
+            Console.WriteLine($"name : {Name} and age = {age}");
+        }
+        class student : Human
+        {
+            public double grades;
+            public student(string name, int age, double grades) : base(name, age)
+            {
+                this.grades = grades;
+            }
+            public override void printinfo()
+            {
+                Console.WriteLine("student");
+                base.printinfo();
+                Console.WriteLine($"grades ={grades}");
+            }
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Human h1 = new Human("osama", 19);
+                h1.printinfo();
+                student s1 = new student("os", 23, 98);
+                s1.printinfo();
+
+            }
+        }
+
+    }
+}
